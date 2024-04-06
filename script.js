e47815efd5d2d34107417f95abe3bc40a6f11899
ankurweb31 <ankur.s3101@gmail.com>
2024-04-06 18:59:00 +0530

    changes did

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $('#hamburger').click(function() {
-      $('#hamburgerMenu').toggleClass('open');
+  $('.hamburger').click(function() {
+      $('.hamburger-menu').toggleClass('active');
   });
 
+  // Close hamburger menu when a nav item is clicked
   $('.hamburger-menu a').click(function() {
-      $('#hamburgerMenu').removeClass('open');
+      $('.hamburger-menu').removeClass('active');
   });
 });
