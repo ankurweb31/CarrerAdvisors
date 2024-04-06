cfbf2756d82336a74a84bbdd7ad8bf4ea3a1cc01
ankurweb31 <ankur.s3101@gmail.com>
2024-04-06 18:15:15 +0530

    change

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
-// // Hamburger Menu Functionality
-// const hamburger = document.getElementById('hamburger');
-// const hamburgerMenu = document.getElementById('hamburgerMenu');
+$(document).ready(function() {
+  $('#hamburger').click(function() {
+      $('#hamburgerMenu').toggleClass('open');
+  });
 
-// hamburger.addEventListener('click', () => {
-//     hamburgerMenu.style.display === 'none' || hamburgerMenu.style.display === ''
-//         ? hamburgerMenu.style.display = 'flex'
-//         : hamburgerMenu.style.display = 'none';
-// });
-
-// // Smooth Scrolling
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-// Hamburger Menu Functionality
-// const hamburger = document.getElementById("hamburger");
-// const hamburgerMenu = document.getElementById("hamburgerMenu");
-
-// hamburger.addEventListener("click", () => {
-//   hamburgerMenu.style.display === "none" || hamburgerMenu.style.display === ""
-//     ? (hamburgerMenu.style.display = "flex")
-//     : (hamburgerMenu.style.display = "none");
-// });
-
-// Smooth Scrolling
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+  $('.hamburger-menu a').click(function() {
+      $('#hamburgerMenu').removeClass('open');
   });
 });
-// Toggle hamburger menu
-const hamburger = document.querySelector('.hamburger');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('open');
-});
-
